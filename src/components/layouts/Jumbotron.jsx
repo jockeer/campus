382408fc0506cf2a8dd1f8672c18e7aa0929a622
Daniel Gorianz <aalbaafikaa@gmail.com>
2021-03-31 18:48:10 -0400

    se temino el responsive del jumbotron

--- src/components/layouts/Jumbotron.jsx
+++ src/components/layouts/Jumbotron.jsx
@@ -6,25 +6,25 @@ import { FaFacebook, FaTwitter, FaYoutube, FaInstagramSquare } from 'react-icons
 const Jumbotron = () => {
     return ( 
         <>
-        <div className="h-48 w-100 border-2 border-black">
-            <div className="container mx-auto xs: p-0  sm:p-5 border-2 border-red-500 h-full flex justify-between flex-wrap  ">
-                <figure className="border-2 xs: text-center border-yellow-500">
-                    <img className="xs: h-25 sm:h-32" src={Logo} alt="Logo Campus"/>
+        <div className="xs: h-auto sm:h-50 w-100">
+            <div className="container mx-auto xs: p-0   sm:p-5  h-full sm:flex justify-between  ">
+                <figure className="xs: text-center">
+                    <img className="xs: h-15 sm:h-24 md:h-32" src={Logo} alt="Logo Campus"/>
                     <h2 className="xs: text-center xs: italic xs: text-gray-500 xs: px-5 xs: font-medium sm:text-left">Centro Tecnologico de Desarrollo Estudiantil</h2>
                 </figure>
-                <div className="redes flex flex-col justify-center items-center border-2 border-green-500">
-                    <h2 className="mb-2 font-bold text-2xl">Visitamos en</h2>
-                    <div className="flex w-96 text-5xl justify-around">
-                        <a href="https://www.google.com/" target="_blank" rel="noreferrer" className="facebook">
+                <div className="redes flex flex-col justify-center items-center  sm:w-72 md:w-72 lg:w-96">
+                    <h2 className="xs: mb-2 xs: text-base xs: font-bold sm:text-2xl">Visitamos en</h2>
+                    <div className="xs: flex xs: justify-around xs: flex-wrap  sm:w-64 sm: text-5xl  md:w-72">
+                        <a href="https://www.google.com/" target="_blank" rel="noreferrer" className="facebook xs: logo-size-xs sm:text-5xl">
                             <FaFacebook/>
                         </a>
-                        <a href="https://www.google.com/" target="_blank" rel="noreferrer" className="twitter">
+                        <a href="https://www.google.com/" target="_blank" rel="noreferrer" className="twitter xs: logo-size-xs sm:text-5xl">
                             <FaTwitter/>
                         </a>
-                        <a href="https://www.google.com/" target="_blank" rel="noreferrer" className="youtube">
+                        <a href="https://www.google.com/" target="_blank" rel="noreferrer" className="youtube xs: logo-size-xs sm:text-5xl">
                             <FaYoutube/>
                         </a>
-                        <a href="https://www.google.com/" target="_blank" rel="noreferrer" className="instagram">
+                        <a href="https://www.google.com/" target="_blank" rel="noreferrer" className="instagram xs: logo-size-xs sm:text-5xl">
                             <FaInstagramSquare/>
                         </a>
                     </div>
